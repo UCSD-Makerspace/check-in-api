4063d8bf387997627a8bd849da85d3f30d17d0b4
Timothy <timothyrwashburn@gmail.com>
2026-05-07 20:35:19 -0700

    remove tests more time would need to be spent implemnting actually effective tests if this is a priority

--- tests/test_health.py
+++
@@ -1,7 +0,0 @@
-def test_health(client):
-    c, _ = client
-    resp = c.get("/health")
-    assert resp.status_code == 200
-    data = resp.json()
-    assert data["status"] == "healthy"
-    assert "timestamp" in data
